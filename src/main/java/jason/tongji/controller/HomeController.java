a7381c7ec96588e79ef30407c9678655b66c1f52
lincolnmi <1281435925@qq.com>
2016-03-28 12:31:20 +0800

    add some controllers

---
+++ src/main/java/jason/tongji/controller/HomeController.java
@@ -0,0 +1,14 @@
+package jason.tongji.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Created by Jason on 2016/3/27.
+ */
+public class HomeController extends Controller{
+
+    public void index() {
+
+    }
+
+}
