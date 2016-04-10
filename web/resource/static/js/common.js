55c2845f18a1adabb3073b1a096f8002c3454286
lincolnmi <1281435925@qq.com>
2016-04-10 23:21:14 +0800

    revise some css

---
+++ web/resource/static/js/common.js
@@ -0,0 +1,20 @@
+seajs.config({
+  alias: {
+    '$': 'jquery/jquery/1.11.1/jquery',
+    'jquery': 'jquery/jquery/1.11.1/jquery',
+    'bootstrap': 'gallery/bootstrap/3.2.0/bootstrap',
+    'echarts': 'echarts/2.0.4/echarts-plain.js',
+    'echarts-map': 'echarts/2.0.4/echarts-plain-map.js',
+    'common': 'pm25/common.js',
+    'tabs': 'arale/switchable/1.0.2/tabs',
+    'switchable': 'jquery/plugin/jquery.switchable.min',
+    'tinyscrollbar': 'jquery/tinyscrollbar/jquery.tinyscrollbar.js',
+    'autocomplete': 'jquery/plugin/jquery.autocomplete',
+    'cookie': 'arale/cookie/1.0.2/cookie',
+    'list': 'gallery/list/1.1.1/list',
+    'mapbox': 'jquery/plugin/jquery.mapbox',
+    'dadadan': 'pm25/dadadan',
+    'danmu': 'pm25/danmu/CommentCoreLibrary.min',
+    'danmuxml': 'pm25/danmu/libxml'
+  }
+});
