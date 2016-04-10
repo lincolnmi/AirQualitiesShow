94a80fbec46744f4934c9a486486fd09b854f86d
lincolnmi <1281435925@qq.com>
2016-04-10 17:01:08 +0800

    add city prefix

--- src/main/java/jason/tongji/model/City.java
+++ src/main/java/jason/tongji/model/City.java
@@ -1,14 +1,19 @@
 package jason.tongji.model;
 
+import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Record;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Jason on 2016/3/24.
  */
 public class City extends Model<City> {
     public static final City dao = new City();
+    private static HashMap<String,ArrayList<String>> cityPrefixes;
 
     public int getCityId(String cityName) {
         String sql = "select * from cities where city_name= '" + cityName +"'";
@@ -23,4 +28,26 @@ public class City extends Model<City> {
         ArrayList<City> city = (ArrayList<City>) City.dao.find(sql);
         return city.get(0).getStr("city_name");
     }
+
+    public static HashMap<String,ArrayList<String>> getCityPrefixes() {
+        if (cityPrefixes==null) {
+            String sql = "select * from cities";
+            List<Record> records = Db.find(sql);
+            cityPrefixes = new HashMap<String, ArrayList<String>>();
+            for (Record record:records) {
+                String city = record.get("city_name");
+                String city_prefix = record.get("city_prefix");
+                ArrayList<String> cities = null;
+                if (cityPrefixes.containsKey(city_prefix)) {
+                    cities = cityPrefixes.get(city_prefix);
+                } else {
+                    cities = new ArrayList<String>();
+                }
+                cities.add(city);
+                cityPrefixes.put(city_prefix,cities);
+            }
+        }
+        return cityPrefixes;
+    }
+
 }
